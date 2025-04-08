f5180addd2b37f1f798244f54c18fe068c0fdebd
tetriv0217 <tetriv0217@gmail.com>
2025-04-08 22:52:59 +0530

    This and Super is done

--- This_Super_8/Demo1.java
+++ This_Super_8/Demo1.java
@@ -1,7 +1,64 @@
 package This_Super_8;
 
+// Super is always there even if you dont mention it.
+// It is the first statment when the constructor is called.
+
+
+// Every class Extends the Object class
+// class A extends Object *Hidden*
+class A{
+
+    public A(){
+        super();
+        System.out.println("in A");
+    }
+    public A(int n){
+        super();
+        System.out.println("in A int");
+    }
+
+}
+class B extends A{
+    public B(){
+        
+        System.out.println("in B");
+    }
+    public B(int n){
+        this();
+        // super(n);//for first two output
+        //This was done to call the parameterized constructor
+        // If there is no constructor then it will show error
+        System.out.println("in B int");
+    }
+}
 public class Demo1 {
     public static void main(String[] args) {
+        // Code 1
+        // B obj = new B());
         
+        //This calls the A constructor as well even though it was't called, because of super(),it only calls the default constructor
+        
+        /*
+        This will give output
+        in A
+        in B
+        */
+
+        // System.out.println("--------------");
+        
+        // Code 2
+
+        // B obj1 = new B(3);
+        // /*
+        // This will give output
+        // in A int
+        // in B int
+        // */
+        // System.out.println("--------------");
+        
+        // Code 3
+
+        // B obj1 = new B(5);//This calls the A constructor as well even though it was't called, because of super(),it only calls the default constructor
+         
     }
 }
---
+++ This_Super_8/Notes/Notes.md
@@ -0,0 +1,14 @@
+# Super keyword in java
+`Every class in java extends the Object class by default.`
+`The Object class is the parent class of all classes in Java`
+
+- The super keyword in Java is a reference variable that is used to refer to the immediate parent class object.
+- It is used to access the members (fields and methods) of the parent class.
+- It can be used to call the parent class constructor, access parent class methods, and access parent class fields.
+- If you want to call parameterized constructor of parent class, you can use super keyword with parameters.
+- The super keyword can be used in the following ways:
+  - To call the parent class constructor.
+  - To access the parent class methods.
+  - To access the parent class fields.
+  - To call the parent class methods that are overridden in the child class.
+
