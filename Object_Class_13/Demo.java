5414918ff1b6b10d910abf19565d01a0f491cf8c
tetriv0217 <tetriv0217@gmail.com>
2025-05-11 16:56:54 +0530

    Done with object class

--- Object_Class_13/Demo.java
+++ Object_Class_13/Demo.java
@@ -3,9 +3,34 @@ package Object_Class_13;
 class Laptop{
     String model;
     int price;
-    public String toString(){
-        return model + ":" + price;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + price;
+        return result;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    }
+    
+    
 }
 
 public class Demo {
